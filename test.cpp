5eedb35a587a140ab7a2caf432b4dff9f627bf3c
Nicholas Tindle <nickandmason@hotmail.com>
2018-12-05 01:48:40 -0600

    Add convert String Grid to Int Grid

--- test.cpp
+++ test.cpp
@@ -16,21 +16,39 @@ using namespace std;
 
 //Vector of ints of the import 
 vector<string> grid;
-vector<string> objects; 
 
+//Temporaryly holds a grid so we can read vector<int> objects and not worry
+vector<int> tempGrid;
+//Holds our objects
+vector<int> objects; 
+
+//general input string
 string inputStr;
 
+//Name of input file
 string filename;
 
+
+//numberOfObjects in vector<int> objects
 int numberOfObjects;
+//tempNumberOfObjects for vector<int> tempGrid
+int tempNumberOfObjects;
+//Side length of all of grid
 int sideLength;
 
 
 //Reads in the grid into inputGrid
 void readInGrid(string inputFileName);
 
-//print the vector 
+//print the vector<string> and vector<int> grids
 void printGrid(vector<string> inputGrid);
+void printGrid(vector<int> inputGrid);
+
+//Turns a vector<string> grid into a vector<int> grid
+void convertStringGridToIntGrid(vector<string> inputGrid);
+
+//Turns a vector<int> grid into a vector<int> objects grid
+void convertGridToObjects(vector<int> inputGrid);
 
 int main()
 {
@@ -42,9 +60,18 @@ int main()
 
     readInGrid(filename);
     
+    cout<<"Print string Grid"<<endl;
     printGrid(grid);
     
-    cout<<"Hit enter to close the program"<<endl;
+    convertStringGridToIntGrid(grid);
+
+    cout<<"print Temp Grid"<<endl;
+    printGrid(tempGrid);
+
+    cout<<"Print objects grid"<<endl;
+    printGrid(objects);
+
+    cout<<"Hit anykey to close the program"<<endl;
     cin>>inputStr;
 }
 
@@ -60,7 +87,7 @@ void readInGrid(string inputFileName){
         while(!inputFile.eof())
         {
             //for each line, add one to the side Length
-            sideLength++;
+           
             getline(inputFile,inputStr);
             istringstream ss(inputStr);
             string segment;
@@ -70,7 +97,11 @@ void readInGrid(string inputFileName){
                 cout<<segment<<endl;
                 grid.push_back(segment);
             }
+            cout<<endl;
+            sideLength++;
         }
+        //to correct for an extra loop of sideLength
+        sideLength--;
     }
     else
     {
@@ -78,9 +109,10 @@ void readInGrid(string inputFileName){
     }
 }
 
+//Prints the vector<string> grid 
 void printGrid(vector<string> inputGrid)
 {
-    
+    cout<<"Side Length is "<<sideLength<<endl;
     for(int i = 0; i < inputGrid.size(); i++)
     {
        //If not first loop, print an enter after every side Length
@@ -90,5 +122,44 @@ void printGrid(vector<string> inputGrid)
         }
         cout<<inputGrid.at(i)<<" ";
     }
+    cout<<endl;
+}
+//prints the vector<int> grid
+void printGrid(vector<int> inputGrid)
+{
+    cout<<"Side Length is "<<sideLength<<endl;
+    for(int i = 0; i < inputGrid.size(); i++)
+    {
+       //If not first loop, print an enter after every side Length
+        if(i!=0&&i%sideLength==0)
+        {
+            cout<<endl;
+        }
+        cout<<inputGrid.at(i)<<" ";
+    }
+    cout<<endl;
+}
+
+//Turns a vector<string> grid into a vector<int> grid
+void convertStringGridToIntGrid(vector<string> inputGrid)
+{
+    for(const auto& val : inputGrid)
+    {
+        tempGrid.push_back(stoi(val));
+    }
+    objects=tempGrid;
+}
+
+//Turns a vector<int> grid into a vector<int> objects grid
+void convertGridToObjects(vector<int> inputGrid)
+{
+
+  
+  for(int i = 0; i < inputGrid.size(); i++)
+  {
+      if(i==0)
+      
+  }
+  
     
 }
